d867ba649070ea766b1b84281a0a307668927916
Michał Dzieliński <dzielinski.michal@gmail.com>
2023-08-31 11:51:37 +0200

    In a halfway

--- app/views.py
+++ app/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
-
-
+from django.shortcuts import render, redirect
+from .models import Book
+from .forms import  BookCreate
+from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'library.html')
+    shelf = Book.objects.all()
+    context = {'shelf': shelf}
+    return render(request, 'library.html', context)
+
+def upload(request):
+    upload = BookCreate()
+    if request.method == 'POST':
+        upload = BookCreate(request.POST, request.FILES)
+        if upload.is_valid():
+            upload.save()
+            return redirect('index')
+        else:
+            return HttpResponse(""" Something went wrong """)
+    else:
+        return render(request, 'upload_form.html', {'upload_form': upload})
